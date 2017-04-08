d7a3502768470ec6c37116b81b7aff18f08b0d44
iuresti <iuresti@skytouchtechnology.com>
2017-04-08 01:06:39 -0500

    Add auto-validation to array sort result

---
+++ selection/test_avg_case.py
@@ -0,0 +1,17 @@
+import time
+from random import randint
+
+from src.selection_algorithm import selection_sort
+from src.validations import validate_sorted_array
+
+input_array = [randint(0,10000) for _ in range(10000)]
+
+start = time.time()
+selection_sort(input_array)
+end = time.time()
+print('Time: ', end - start)
+
+if validate_sorted_array(input_array):
+    print ("Sort OK")
+else:
+    print ("Failing Sort")
--- selection/test_worst_case.py
+++ selection/test_worst_case.py
@@ -1,6 +1,7 @@
 import time
 
 from src.selection_algorithm import selection_sort
+from src.validations import validate_sorted_array
 
 input_array = [x for x in range(10000, 0, -1)]
 
@@ -8,3 +9,8 @@ start = time.time()
 selection_sort(input_array)
 end = time.time()
 print('Time: ', end - start)
+
+if validate_sorted_array(input_array):
+    print ("Sort OK")
+else:
+    print ("Failing Sort")
