ac68665ebfbee51946d3942f098446c953b911fb
iuresti <iuresti@skytouchtechnology.com>
2017-04-18 12:32:14 -0500

    Format variable names

--- selection/src/selection_algorithm.py
+++ selection/src/selection_algorithm.py
@@ -1,15 +1,16 @@
 
 def selection_sort(input_list):
     for i in range(len(input_list) - 1):
-        positionOfMin = i
-        for j in range(i + 1, len(input_list)):
-            if input_list[j] < input_list[positionOfMin]:
-                positionOfMin = j
+        position_of_min = i
 
-        if positionOfMin != i:
+        for j in range(i + 1, len(input_list)):
+            if input_list[j] < input_list[position_of_min]:
+                position_of_min = j
+
+        if position_of_min != i:
             temp = input_list[i]
-            input_list[i] = input_list[positionOfMin]
-            input_list[positionOfMin] = temp
+            input_list[i] = input_list[position_of_min]
+            input_list[position_of_min] = temp
 
     return input_list;
 
